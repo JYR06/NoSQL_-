865ba5d3f135cc5fa55a524d057eef64d8d2334b
tjdus7 <psyb062@gmail.com>
2026-05-30 00:07:19 +0900

    새 프로젝트 now_app

--- android/app/src/main/kotlin/com/example/flutter_project_now_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_project_now_app/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.flutter_project_now_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+}
